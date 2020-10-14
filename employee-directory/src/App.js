98ce3e64443564e42ab062d4a884a2ba05d27270
Ashley Holanda <aholanda12@gmail.com>
2020-10-13 20:17:51 -0400

    updated app file to correspond to employee data

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -2,35 +2,35 @@ import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import friends from "./friends.json";
+import employees from "./employees.json";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    employees
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
+  // removeFriend = id => {
+  //   // Filter this.state.friends for friends with an id not equal to the id being removed
+  //   const friends = this.state.friends.filter(friend => friend.id !== id);
+  //   // Set this.state.friends equal to the new friends array
+  //   this.setState({ friends });
+  // };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
+        <Title>Employee Directory</Title>
+        {this.state.employees.map(emp => (
           <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+            id={emp.id}
+            key={emp.id}
+            name={emp.name}
+            image={emp.image}
+            department={emp.department}
+            email={emp.email}
+            phone={emp.phone}
           />
         ))}
       </Wrapper>
