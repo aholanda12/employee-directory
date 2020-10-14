c2e33a506f26bf0d8a746ab776c5081cc85f007f
Ashley Holanda <aholanda12@gmail.com>
2020-10-14 17:09:29 -0400

    began working on onchange event for search to filter

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -12,12 +12,19 @@ class App extends Component {
     employees
   };
 
-  // removeFriend = id => {
-  //   // Filter this.state.friends for friends with an id not equal to the id being removed
-  //   const friends = this.state.friends.filter(friend => friend.id !== id);
-  //   // Set this.state.friends equal to the new friends array
-  //   this.setState({ friends });
-  // };
+  handleSearch = event => {
+    console.log(event)
+    // // Filter this.state.friends for friends with an id not equal to the id being removed
+    // const friends = this.state.friends.filter(friend => friend.id !== id);
+    // // Set this.state.friends equal to the new friends array
+    // this.setState({ friends });
+  };
+
+  componentDidMount() {
+        this.setState({ employees });
+      };
+
+  
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
--- employee-directory/src/components/Search/index.js
+++ employee-directory/src/components/Search/index.js
@@ -1,12 +1,72 @@
 import React from "react";
 import "./style.css";
 
-function Search() {
-  return<form className="search">
-  <input
-      type="text"
-      placeholder="Search Name"/>
-</form>
+class Search extends React.Component {
+
+  state = {
+      searchTerm: "",
+      filteredEmployees: []
+  };
+
+  componentDidMount() {
+      if (this.state.filteredEmployees.length < 1) {
+          this.setState({
+              filteredEmployees: this.props.employees
+          })
+      }
+  }
+
+
+  handleInputChange = event => {
+      this.setState({
+          searchTerm: event.target.value
+      });
+      let userTyped = event.target.value;
+      console.log(userTyped)
+      const filteredList = this.props.employees.filter((item) => {
+          let values = item.name + item.department + item.name.last + item.gender + item.dob.age + item.email + item.cell;
+          return values.indexOf(userTyped) !== -1;
+
+      });
+
+      this.setState({
+          filteredEmployees: filteredList
+
+      });
+  }
+
+  render() {
+      return (
+          <div>
+              {/* {this.fillList} */}
+              <form className="form">
+                  <input
+                      value={this.state.searchTerm}
+                      className="search"
+                      onChange={event => this.handleInputChange(event)}
+                      type="text"
+                      placeholder="Search"
+                  />
+              </form>
+          </div>
+
+      );
+  }
 }
 
+
 export default Search;
+
+// function Search(event) {
+//   return<form className="search">
+//   <input
+//       type="text"
+//       placeholder="Search Name"
+//       onChange={handleSearch(event)}
+//       />
+// </form>
+// }
+
+
+
+// export default Search;
