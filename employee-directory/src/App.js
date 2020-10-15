d9d211307b186a59a6db5b01a6c204b7bc32c3f6
Ashley Holanda <aholanda12@gmail.com>
2020-10-15 18:46:13 -0400

    updated function to filter upon search

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -12,39 +12,26 @@ class App extends Component {
     employees
   };
 
-  handleSearch = event => {
-    console.log(event)
-    // // Filter this.state.friends for friends with an id not equal to the id being removed
-    // const friends = this.state.friends.filter(friend => friend.id !== id);
-    // // Set this.state.friends equal to the new friends array
-    // this.setState({ friends });
-  };
-
   handleInputChange = event => {
-    // this.setState({
-    //     searchTerm: event.target.value
-    // });
-    // let userTyped = event.target.value;
-    console.log(event.target.value)
-  //   const filteredList = this.props.employees.filter((item) => {
-  //       let values = item.name + item.department + item.name.last + item.gender + item.dob.age + item.email + item.cell;
-  //       return values.indexOf(userTyped) !== -1;
-
-  //   });
-
-  //   this.setState({
-  //       filteredEmployees: filteredList
-
-  //   });
-}
+    const searchText = event.target.value
+    console.log(searchText)
+    const employees = this.state.employees.filter(employee => {
+      if (employee.name.toLowerCase().includes(searchText.toLowerCase())) {
+        return true;
+      }
+      return false;
+    });
+    console.log(employees)
+    this.setState({ employees })
+  }
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
         <Title>Employee Directory</Title>
-        <Search 
-        handleInputChange={this.handleInputChange} />
+        <Search
+          handleInputChange={this.handleInputChange} />
         <Header></Header>
         {this.state.employees.map(emp => (
           <EmployeeCard
